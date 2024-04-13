1e1ab92cd2502e3fb474c2f7828313ecef726f6f
Diego Cerqueira <135269786+Diego-Cerqueira@users.noreply.github.com>
2024-04-13 14:53:20 -0300

    add exercícios
    exercícios do 06-08

---
+++ Curso em Vídeo Exercícios/Exercício 006.py
@@ -0,0 +1,9 @@
+from math import sqrt as quad
+numero = int(input('Digite um néumero'))
+print(f'''
+O dobro de {numero} é {numero*2}
+O triplo de {numero} é {numero*3}
+A raiz de {numero} é {quad(numero)}
+''')
+
+
---
+++ Curso em Vídeo Exercícios/Exercício 008.py
@@ -0,0 +1,11 @@
+m = float(input('Digite uma medida(metros): '))
+print(f'''
+A medida de {m}m corresponde a:
+{m/1000}km
+{m/100}hm
+{m/10}dam
+{m*10}dm
+{m*100}cm
+{m*1000}mm
+''')
+
