49b2f49e6f5548dd99585cb1e625f7836befb3fd
Diego Cerqueira <135269786+Diego-Cerqueira@users.noreply.github.com>
2024-04-13 15:20:21 -0300

    add exercioes

---
+++ Curso em Vídeo Exercícios/Exercício 010.py
@@ -0,0 +1,3 @@
+#Programa simples de conversão do real para dólar baseando no valor do Dolár no dia 13/04/2024#
+v = float(input('Digite o valor disponível R$:'))
+print(f'Com o valor {v} é possível comprar U$$:{v/5.08:.2f}')
