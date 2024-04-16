06c40e8057ed9d6a2723d8ce44f0403d360545ee
Diego Cerqueira <135269786+Diego-Cerqueira@users.noreply.github.com>
2024-04-15 21:27:12 -0300

    add exercícios
    11

---
+++ Curso em Vídeo Exercícios/exercício 011.py
@@ -0,0 +1,7 @@
+altura = float(input('Digite a altura: '))
+largura = float(input('Digite a largura :'))
+area = altura * largura
+print(f'''
+Sua parede tem a dimenção de {altura}m x {largura}m sua área é de {area}m2
+Vai precisar de {area/2}L de tinta
+''')
