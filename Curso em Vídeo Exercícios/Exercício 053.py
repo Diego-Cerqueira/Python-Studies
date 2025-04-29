8f624d541b0fcfc1b019377f152defad62cc5e53
Diego Cerqueira <135269786+Diego-Cerqueira@users.noreply.github.com>
2025-04-29 12:48:59 -0300

    Update Exercício 053.py

--- Curso em Vídeo Exercícios/Exercício 053.py
+++ Curso em Vídeo Exercícios/Exercício 053.py
@@ -3,6 +3,6 @@ palavra = input('Digite uma palavra: ')
 poli = palavra[-1::-1]
 print(f'A palavra {palavra} ao contrario fica {poli} portanto', end=' ')
 if palavra == poli:
-    print('é um palindormo')
+    print('é um Palíndromo')
 else:
-    print('não é um Palindromo')
+    print('não é um Palíndromo')
